26c656034702c369c6ebe62aa7cd820c07f57eeb
jongisli <jongisli@gmail.com>
2013-12-16 16:34:50 +0100

    Implemented the Replicator and partially the ReplicationTask

--- com/acertainbookstore/business/CertainBookStoreReplicationTask.java
+++ com/acertainbookstore/business/CertainBookStoreReplicationTask.java
@@ -2,21 +2,28 @@ package com.acertainbookstore.business;
 
 import java.util.concurrent.Callable;
 
+import com.acertainbookstore.utils.BookStoreMessageTag;
+
 /**
  * CertainBookStoreReplicationTask performs replication to a slave server. It
  * returns the result of the replication on completion using ReplicationResult
  */
 public class CertainBookStoreReplicationTask implements
 		Callable<ReplicationResult> {
+	
+	private String slaveServer;
+	private ReplicationRequest request;
 
-	public CertainBookStoreReplicationTask() {
-		// TODO Auto-generated constructor stub
+	public CertainBookStoreReplicationTask(String slaveServer, ReplicationRequest request) {
+		this.slaveServer = slaveServer;
+		this.request = request;
 	}
 
 	@Override
 	public ReplicationResult call() throws Exception {
-		// TODO Auto-generated method stub
+		BookStoreMessageTag messageTag = request.getMessageType();
 		return null;
+		
 	}
 
 }
